f6cd28ffe1f4ef2ffdad301a95c00de2c0c75472
soaint <30303456+elkinprog@users.noreply.github.com>
2022-11-28 12:47:43 -0500

    Modificacion controller AtencionGrupal post

--- ORIENTACION_INFORMACION_Back/WebApi/Controllers/AtencionGrupalController.cs
+++ ORIENTACION_INFORMACION_Back/WebApi/Controllers/AtencionGrupalController.cs
@@ -29,31 +29,38 @@ namespace WebApi.Controllers
 
             try
             {
+                var atenciongrupalDTO = _mapper.Map<AtencionGrupal>(atenciongrupal);
+                atenciongrupalDTO.DtFechaRegistro = DateTime.Now;
+
+                bool guardo = await _service.CreateAsync(atenciongrupalDTO);
+
+                if (!guardo)
+                {
+                    response = new { Titulo = "Algo salio mal", Mensaje = "No se pudo guardar atención grupal", Codigo = HttpStatusCode.BadRequest };
+                }
+
+
                 string rutaInicial = Environment.CurrentDirectory;
                 var nombreArchivo = files[0].FileName;
                 var rutaArchivo = rutaInicial + "/Upload/" + nombreArchivo;
+                var archivoArray = nombreArchivo.Split(".");
+                var extencion = archivoArray[archivoArray.Length - 1];
 
-                byte[] archivo = System.IO.File.ReadAllBytes(rutaArchivo);
+                FileInfo file = new FileInfo(rutaArchivo);
+
+                //byte[] archivo = System.IO.File.ReadAllBytes(rutaArchivo);
 
 
-                if (archivo.Length > 1048576)
-                {
-                    response = new { Titulo = "Algo salio mal", Mensaje = "El archio PDF no debe superar los 2 megabytes", Codigo = HttpStatusCode.BadRequest };
-                    return StatusCode((int)response.Codigo, response);
-                }
+                //if (files.Capacity > 1048576)
+                //{
+                //    response = new { Titulo = "Algo salio mal", Mensaje = "El archio PDF no debe superar los 2 megabytes", Codigo = HttpStatusCode.BadRequest };
+                //    return StatusCode((int)response.Codigo, response);
+                //}
 
                 if (files.Count == 0)
                 {
                     response = new { Titulo = "Algo salio mal", Mensaje = "No se recibio el archivo ", Codigo = HttpStatusCode.BadRequest };
                 }
-
-
-                //string rutaInicial = Environment.CurrentDirectory;
-                //var nombreArchivo = files[0].FileName;  
-
-
-                var archivoArray = nombreArchivo.Split(".");
-                var extencion = archivoArray[archivoArray.Length - 1];
 
 
                 if (extencion != "pdf")
@@ -63,8 +70,7 @@ namespace WebApi.Controllers
                 }
                 else
                 {
-                    //var rutaArchivo = rutaInicial + "/Upload/" + nombreArchivo;
-
+                   
                     if (files.Count == 1)
                     {
                         System.IO.File.Delete(rutaArchivo);
@@ -83,18 +89,7 @@ namespace WebApi.Controllers
                response = new { Titulo = "Algo salio mal", Mensaje = "Error cargando el archivo PDf", Codigo = HttpStatusCode.BadRequest };
                 return StatusCode((int)response.Codigo, response);
             }
-            
-            var atenciongrupalDTO = _mapper.Map<AtencionGrupal>(atenciongrupal);
-
-            atenciongrupalDTO.DtFechaRegistro = DateTime.Now;
-
-            bool guardo = await _service.CreateAsync(atenciongrupalDTO);
-
-            if (!guardo)
-            {
-                response = new { Titulo = "Algo salio mal", Mensaje = "No se pudo guardar atención grupal", Codigo = HttpStatusCode.BadRequest };
-            }
-         
+           
             var modelResponse = new ModelResponse<AtencionGrupalDTO>(response.Codigo, response.Titulo, response.Mensaje, atenciongrupal);
             return StatusCode((int)modelResponse.Codigo, modelResponse);
         }
